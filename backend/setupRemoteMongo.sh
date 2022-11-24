1797fc8724e2e95bd6972449fd83cc0712b0ca78
VIMAL S <99335637+Vimal567@users.noreply.github.com>
2022-11-24 23:20:00 +0530

    Update setupRemoteMongo.sh

--- backend/setupRemoteMongo.sh
+++ backend/setupRemoteMongo.sh
@@ -1,2 +1,2 @@
 # Setup file template to upload data to MongoDB Atlas
-mongoimport --uri "mongodb+srv://vimal:vimal567@xflix-node.tpmpvzt.mongodb.net/?retryWrites=true&w=majority" --drop --collection video --file data/export_xflix_videos.json
+mongoimport --uri "mongodb+srv://vimal:vimal567@xflix-node.tpmpvzt.mongodb.net/?retryWrites=true&w=majority" --drop --collection videos --file data/export_xflix_videos.json
