77deefcf8cc27717d415225e48fdf85b2e2036ab
VIMAL S <99335637+Vimal567@users.noreply.github.com>
2022-11-24 19:22:33 +0530

    Update config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -9,7 +9,7 @@ const envVarsSchema = Joi.object()
     NODE_ENV: Joi.string()
       .valid("production", "development", "test")
       .required(),
-    PORT: Joi.number().default(3000),
+      SERVER_PORT: Joi.number().default(3000),
     MONGODB_URL: Joi.string().required().description("Mongo DB url"),
   })
   .unknown();
@@ -22,7 +22,7 @@ if (error) {
 
 module.exports = {
   env: envVars.NODE_ENV,
-  port: envVars.PORT,
+  SERVER_PORT: envVars.SERVER_PORT,
   // Set mongoose configuration
   mongoose: {
     url: envVars.MONGODB_URL + (envVars.NODE_ENV === "test" ? "-test" : ""),
