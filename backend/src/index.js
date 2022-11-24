6dce9e701bda4871937e14930424015350e28aed
VIMAL S <99335637+Vimal567@users.noreply.github.com>
2022-11-24 20:17:57 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const app = require("./app");
 const config = require("./config/config");
 
 let server;
+console.log(config.mongoose.url)
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
     console.log("Connected to MongoDB");
     server = app.listen(config.port, () => {
