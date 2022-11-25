c36f030a1b77d46e0c69934fee649d6d831dbf63
VIMAL S <99335637+Vimal567@users.noreply.github.com>
2022-11-25 14:10:04 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,8 @@ const config = require("./config/config");
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
     console.log("Connected to MongoDB");
-    server = app.listen(config.port, () => {
-      console.log(`App is running on port ${config.port}`);
+    server = app.listen(config.SERVER_PORT, () => {
+      console.log(`App is running on port ${config.SERVER_PORT}`);
     });
     return;
 })
